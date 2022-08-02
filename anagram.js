73ebbcb5593477d1d0d4483e5878308980f2025e
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-02 17:02:39 +0200

    2022-08-02

--- anagram.js
+++ anagram.js
@@ -4,22 +4,32 @@ function makeAnagram(a,b) {
     let two = b.split('').sort().join('').split('');
     console.log("first ",one);
     console.log("second ",two);
-    let filter1 = one.filter((item) => !two.includes(item));
-    let filter2 = two.filter((item2) => !one.includes(item2));
-    console.log("filter1",filter1);
-    console.log("filter2",filter2);
-    let filter3 = filter1.filter((item) => filter2.includes(item));
-    let filter4 = filter2.filter((item2) => filter1.includes(item2));
-    console.log(filter3);
-    console.log(filter4);
-    let red = new Array();
-    red = [];
-    red.push(filter3,filter4);
-    let blue = red.flat();
-    console.log("final",blue);
-    return blue.length;
+    //put identical letters into new array
+    // let three = one.concat(two);
+    // let arr = new Array();
+    // arr = [];
+    // for (let i = 0; i < three.length; i  += 1) {
+    //     if (one[i] === two[i]) {
+    //         arr.push(one[i]);
+    //     }
+    // }
+    // console.log(arr);
+    // let filter1 = one.filter((item) => !two.includes(item));
+    // let filter2 = two.filter((item2) => !one.includes(item2));
+    // console.log("filter1",filter1);
+    // console.log("filter2",filter2);
+    // let three = one.concat(two);
+    
+    // let red = new Array();
+    // red = [];
+    // red.push(filter1,filter2);
+    // let blue = red.flat();
+    // console.log("final",blue);
+    // return blue.length;
+    //console.log(one);
+    //console.log(two);
 }
 
 console.log(makeAnagram('cdefefe','abcded'));
-console.log(makeAnagram('cde','dcf'));
+//console.log(makeAnagram('cde','dcf'));
 //console.log(makeAnagram('fcrxzwscanmligyxyvym','jxwtrhvujlmrpdoqbisbwhmgpmeoke'));
