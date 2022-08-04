c532f6793eeefdff78aec38cf1a47d841dfc4f41
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-04 13:53:54 +0200

    staircase challenge solved

---
+++ staircase.js
@@ -0,0 +1,9 @@
+function staircase(n) {
+    const hash = '#';
+    const space = ' ';
+    for (let i = n, j = 1; i > 0 && j <= n; i -= 1, j += 1) {
+        console.log(space.repeat(i-1) + hash.repeat(j));
+    }
+}
+
+staircase(6);
