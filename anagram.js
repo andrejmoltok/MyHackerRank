4bc2bddb9792831e68e388bb33196a2558bcbca2
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-05 14:00:36 +0200

    timeConv completed, minMaxSum updated, anagram unsolved

--- anagram.js
+++ anagram.js
@@ -1,14 +1,28 @@
 function makeAnagram(a,b) {
     let one = a.split('').sort().join('').split('');
-    let two = b.split('').sort().join('').split('');
+    let two = b.split('').sort().join('').split('');;
     let count = 0;
     
     let filt1 = one.filter((letter) => !two.includes(letter));
     let filt2 = two.filter((letter) => !one.includes(letter));
-    count = filt1.length + filt2.length;
-    return count;
+
+    //filter out letters which are only in one of the arrays
+    // for (let i = 0; i < filt1.length; i +=1) {
+    // if (filt1 === filt2) {
+    //     filt1.pop();
+    //    } else {
+    //     filt2.pop();
+    //    }
+    // } 
+    console.log(filt1.join(''));
+    console.log(filt2.join(''));
+    // count = filt1.length + filt2.length;
+    // return count;
 }
 
 console.log(makeAnagram('cdefe','abcde'));
 console.log(makeAnagram('cde','dcf'));
-//console.log(makeAnagram('fcrxzwscanmligyxyvym','jxwtrhvujlmrpdoqbisbwhmgpmeoke'));
+console.log(makeAnagram('fcrxzwscanmligyxyvym','jxwtrhvujlmrpdoqbisbwhmgpmeoke'));
+
+// fcrxzwscanmligyxyvym
+// jxwtrhvujlmrpdoqbisbwhmgpmeoke
--- timeConversion.js
+++ timeConversion.js
@@ -1,3 +1,18 @@
 function timeConv(s) {
-    
+    let hour = s.substring(0,2);
+    let format = s.substring(2,8);
+    let last = s.substring(8,10);
+    if (hour === String(12) && last === "AM") {
+        return "00" + format;
+    } else if (hour === String(12) && last === "PM") {
+        return hour + format;
+    } else if (hour < String(12) && last === "PM") {
+        return (12 + Number(hour) + format);
+    } else if (hour < 10) {
+        return (hour + format);
+    }
 }
+
+console.log(timeConv('07:05:45AM'));
+console.log(timeConv('07:05:45PM'));
+console.log(timeConv("12:40:22AM"));
