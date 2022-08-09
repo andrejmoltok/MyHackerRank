8bf3f57997e2aa2ac9e20f8b55a5a85100a97f13
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-09 14:33:26 +0200

    solved after many trial-and-error, non-clean code

---
+++ kangaroo.js
@@ -0,0 +1,25 @@
+function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+    // repeat addition until x1 = x2
+    let jumps = Math.abs(x1 - x2) / Math.abs(v2 - v1);
+    if (v1 === v2 && (x1 > x2 || x2 > x1)) {
+        return 'NO';
+    }
+    for (let i = 0; i < jumps; i += 1) {
+        x1 = x1 + v1;
+        x2 = x2 + v2;
+    }
+    if (x1 === x2) {
+        return 'YES';
+    }
+    else
+    {
+        return 'NO';
+    }
+}
+
+console.log(kangaroo(43, 2, 70, 2));
+console.log(kangaroo(0,3,4,2));
+console.log(kangaroo(63, 8, 94, 3));
+console.log(kangaroo(1817, 9931, 8417, 190));
+//console.log(kangaroo(1571, 4240, 9023, 4234));
