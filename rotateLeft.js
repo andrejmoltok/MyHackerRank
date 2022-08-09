0e576c47cfc1b9b7f207619d09a66f63a0ffcec5
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-08-09 15:58:13 +0200

    rotateLeft solved

---
+++ rotateLeft.js
@@ -0,0 +1,10 @@
+function rotLeft(a, d) {
+    // Write your code here
+    for (let i = 1; i <= d; i += 1) {
+        let first = a.shift();
+        a.push(first);
+    }
+    return a;
+}
+
+console.log(rotLeft([1,2,3,4,5],4));
