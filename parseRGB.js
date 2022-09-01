79c7d9fbf6847c33da3eae8502467c20845aeb78
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-01 22:28:37 +0200

    code logic correction

--- parseRGB.js
+++ parseRGB.js
@@ -1,17 +1,19 @@
 function parseRGB(x) {
-    let arr = x.split(',');
-    let first = Number(arr[0].split('(')[1]);
-    let sec = Number(arr[1]);
-    let third = Number(arr[2].split(')')[0]);
-    let toParse = [];
-    toParse.push(first,sec,third);
-    let hex = '#';
-    for (let i = 0; i < toParse.length; i += 1) {
-        if (toParse[i].toString(10) !== Number.isNaN(toParse[i])) {
-            hex += (toParse[i]).toString(16);
-        }
+  const arr = x.split(',');
+  const first = Number(arr[0].split('(')[1]);
+  const sec = Number(arr[1]);
+  const third = Number(arr[2].split(')')[0]);
+  const toParse = [];
+  toParse.push(first, sec, third);
+  let hex = '#';
+  for (let i = 0; i < toParse.length; i += 1) {
+    if (Number(toParse[i].toString(16)) < 10) {
+      hex += '0' + (toParse[i]).toString(16);
+    } else {
+      hex += (toParse[i]).toString(16);
     }
+  }
     return hex;
 }
 
-console.log(parseRGB("RGB(167,39,54)"));
+console.log(parseRGB("RGB(167, 3, 54)"));
