d1f32ec7c5489a53e0ad262decacc930c82f4949
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-15 18:12:49 +0200

    solved with new Array, ternary op, array methods(push,slice,forEach,reduce)

--- subarray_division.js
+++ subarray_division.js
@@ -1,3 +1,21 @@
 function birthday(s, d, m) {
-    
+    let count = 0;
+
+    let arr = new Array();
+    arr = [];
+
+    if (m == 1) {
+        return (s[0] === d) ? 1 : "";
+    } else if (m > 1) {
+        for (let i = 0; i < s.length; i++) {
+            arr.push(s.slice(i,m+i));
+        }
+    }
+
+    arr.forEach(function(v,i,a) {
+        if (v.reduce((p,c) => p+c,0) === d) {count++}});
+
+    return count;
 }
+
+console.log(birthday([2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1],18,7));
