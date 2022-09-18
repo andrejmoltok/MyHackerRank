f280cfd1441c3dd8999899b0965c93d71eb8a59a
andrejmoltok <csokan.andras87@protonmail.ch>
2022-09-18 20:20:14 +0200

    added new example, tried array function approach, no luck

--- diagDiff.js
+++ diagDiff.js
@@ -2,20 +2,20 @@ function diagDiff(arr){
     let rtl = 0;
     let ltr = 0;
     let sum = 0;
+
     for (let i = 0; i < arr.length; i += 1) {
         ltr += arr[i][i];
-    }
+    };
+
     for (let j = arr.length-1; j >= 0; j -=1) {
         rtl += arr[j][arr.length - 1 - j];
-    }
+    };
+    
     sum = Math.abs(rtl - ltr);
+
     return sum;
 }
 
-let arr = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-];
+console.log(diagDiff([[1,2,3],[4,5,6],[7,8,9]]));          //sum = 0
 
-console.log(diagDiff(arr));
+console.log(diagDiff([[3,7,9],[4,1,0],[0,0,2]]));          //sum = 4
