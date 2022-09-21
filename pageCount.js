bf04084f039aff99478ff62545d0e2389f57d371
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-21 23:56:30 +0200

    simple if/else statement solution

--- pageCount.js
+++ pageCount.js
@@ -1,46 +1,21 @@
 // https://www.hackerrank.com/challenges/drawing-book/problem
+// n - number of pages in the book
+// p - page you need to flip to
 
 function pageCount(n, p) {
-    let pages = [];
-    let bookodd;
-    let bookeven;
-    let countUpEven = 0;
-    let countDownEven = 0;
-    let countUpOdd = 0;
-    let countDownOdd = 0;
-    //console.log("Flip to page",p);
-    //console.log("Minimum pages needed to flip,");
-    if (p == n-1) {
+    if (p == 1) {
         return 0;
-    } else if (p % 2 === 0) {
-        for (bookeven = 0; bookeven < p; bookeven+=2) {
-            countUpEven++;
-            //console.log("counter up",countUpEven);
-            //console.log("even pages",bookeven);
-        }
-        for (bookeven = n; bookeven > p; bookeven-=2) {
-            countDownEven++;
-            //console.log("counter down",countDownEven);
-            //console.log("even pages",bookeven);
-        }
-        pages.push([countUpEven,countDownEven]);
-        
-    } else {
-        for (bookodd = 1; bookodd < p; bookodd+=2) {
-            countUpOdd++;
-            console.log("counter up",countUpOdd);
-            console.log("odd pages",bookodd);
-        }
-        for (bookodd = n; bookodd > p; bookodd-=2) {
-            countDownOdd++;
-            console.log("counter down",countDownOdd);
-            console.log("odd pages",bookodd);
-        }
-        pages.push([countUpOdd,countDownOdd]);
+    } else if (p % 2 !== 0 && p == n-1) {
+        return 1;
+    } else if (p % 2 === 0 && p == n-1) {
+        return 0;
+    } else if (p <= (n / 2)) {
+        return Math.floor(p / 2);
+    } else if (p >= (n / 2)) {
+        return Math.floor((n - p) / 2);
     }
-    return Math.min(...pages.flat());
 }
 
 
-console.log(pageCount(6,2));
-console.log(pageCount(5,4));
+console.log(pageCount(7,3));
+console.log(pageCount(6,5));
