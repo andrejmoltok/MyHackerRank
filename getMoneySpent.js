99ce194651e45a148583c7eec133950a303b3f73
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-09-25 16:32:53 +0200

    UNSOLVED YET

---
+++ getMoneySpent.js
@@ -0,0 +1,25 @@
+function getMoneySpent(keyboards, drives, b) {
+    // define variables
+    let sum = [];
+    // addition of keyboards array elements to
+    // drives array elements on-by-one
+    for (let i = 0; i < keyboards.length; i++) {
+        for (let j = 0; j < drives.length; j++) {
+            let val = (keyboards[i]+drives[j]);
+            //store values in a new array
+            sum.push(val);
+        }
+    }
+    // return ha maximum amount which can be spent
+    let exp = Math.max(...((sum.filter((elem) => !(elem > b)))));
+    if (sum.length == 1 && sum > b) {
+        return -1;
+    } else if (exp > b) {
+        return -1;
+    } else {
+        return exp;
+    }
+}
+
+console.log(getMoneySpent([3,1],[5,2,8],10));
+console.log(getMoneySpent([4],[5],5));
