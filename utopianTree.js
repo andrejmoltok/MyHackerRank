0b35c52b32356b9a80a61cd02da9a2ab54b5c414
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-10-08 13:35:18 +0200

    added comments to utopianTree solution

--- utopianTree.js
+++ utopianTree.js
@@ -2,9 +2,15 @@
 
 function utopianTree(n) {
     let H = 1;
+    
+    // if growing cycle (n) equals 0 return 1
     if (n == 0) {
         return H;
     } else {
+        // if growing cycle (n) if larger than 0,
+        // calculate:
+        // odd cycles as (H * 2), spring cycle doubles height,
+        // even cycles as (H + 1), summer cycle grows 1 meter
         for (let i = 1; i <= n; i++) {
             if (i % 2 != 0) {
                 H = H * 2;
