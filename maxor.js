2ce57be3945935d3a1b4ba1c3b584d4802e18b32
Ciocan Pál András <csokan.andras87@protonmail.ch>
2023-03-07 08:18:05 +0100

    Maximizing XOR FASTER version

--- maxor.js
+++ maxor.js
@@ -1,13 +1,38 @@
+// function maximizingXor(l, r) {
+//     // Write your code here
+//     let arr = [];
+//     for (let i = l; i <= r; i++) {
+//         for (let j = i; j <= r; j++) {
+//             //console.log(i,j);
+//             arr.push(i ^ j)
+//         }
+//     }
+//     return arr.sort((a,b) => {return b-a})[0];
+// }
+
+//faster version
 function maximizingXor(l, r) {
     // Write your code here
     let arr = [];
+    let temp = 0;
+    let max = 0;
     for (let i = l; i <= r; i++) {
         for (let j = i; j <= r; j++) {
-            //console.log(i,j);
-            arr.push(i ^ j)
+            let a = i ^ j;
+            if (a > temp) {
+                temp = a;
+                max = temp;
+            }
+            if (a == temp) {
+                temp = a;
+                max = max;
+            }
+            if (a < temp) {
+                continue;
+            }
         }
     }
-    return arr.sort((a,b) => {return b-a})[0];
+    return max;
 }
 
 console.log(maximizingXor(10,15));
